ead77944e77abeafa8f3493f74f1bb549f47ce4b
Parag Zaveri <parag.g.zaveri@gmail.com>
2018-08-07 14:58:04 -0500

    added marker

--- src/index.js
+++ src/index.js
@@ -8,3 +8,10 @@ const map = new mapboxgl.Map({
   zoom: 12, // starting zoom
   style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
 });
+
+const markerDomEl = document.createElement("div"); // Create a new, detached DIV
+markerDomEl.style.width = "32px";
+markerDomEl.style.height = "39px";
+markerDomEl.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";
+
+new mapboxgl.Marker(markerDomEl).setLngLat([-87.6298 , 41.8781]).addTo(map);
